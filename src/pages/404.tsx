0ba1294dfcb9e75cf1bf591f0cfb8875aa84cb3d
rlasso <ronylasso@gmail.com>
2021-02-25 18:24:06 -0500

    feat: [d47fc8a726cc] initial project setup

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "primereact/resources/themes/md-light-indigo/theme.css"
+import "primereact/resources/primereact.min.css"
+import "primeicons/primeicons.css"
+import { Button } from "primereact/button"
+import { Router } from "@reach/router"
+
+const Page404 = Router => (
+  <div>
+    Not found
+  </div>
+)
+
+export default Page404
---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import "primereact/resources/themes/md-light-indigo/theme.css"
+import "primereact/resources/primereact.min.css"
+import "primeicons/primeicons.css"
+import { Button } from "primereact/button"
+import { Router } from "@reach/router"
+
+const Home = Router => (
+  <div>
+    Hello world!
+    <h5>Rounded Icon Buttons</h5>
+    <Button
+      icon="pi pi-bookmark"
+      className="p-button-rounded p-button-secondary"
+    />
+    <Button icon="pi pi-search" className="p-button-rounded p-button-success" />
+    <Button icon="pi pi-user" className="p-button-rounded p-button-info" />
+    <Button icon="pi pi-bell" className="p-button-rounded p-button-warning" />
+    <Button icon="pi pi-heart" className="p-button-rounded p-button-help" />
+    <Button icon="pi pi-times" className="p-button-rounded p-button-danger" />
+    <Button icon="pi pi-check" className="p-button-rounded" />
+  </div>
+)
+
+export default Home
